752152c4350bc1e1f6b53fddd1f7e26324c6bfe8
Beyza Nur Deliktaş <126319075+bnrdelk@users.noreply.github.com>
2024-05-10 20:32:52 +0300

    assignment#10

---
+++ InclassActivities/Activity10/Activity10_Slider/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        randomStart: true, 
+        moveSlides: 1, 
+        minSlides: 1, 
+        maxSlides: 1, 
+        slideWidth: 250,
+        slideMargin: 10,
+        pause: 3000,
+        captions: true,
+        pager: true,
+        pagerType: 'short'
+    });
+});
+
+		
